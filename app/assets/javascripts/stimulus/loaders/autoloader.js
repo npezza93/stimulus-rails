65ca2a2e6b2c1eca852c8586b346675650d71bbe
Nick Pezza <pezza@hey.com>
2021-01-15 20:57:52 -0500

    log error

--- app/assets/javascripts/stimulus/loaders/autoloader.js
+++ app/assets/javascripts/stimulus/loaders/autoloader.js
@@ -23,7 +23,7 @@ function autoloadControllers(element) {
       application.register(controllerName, controllerModule.default)
 
       loaded[controllerName] = true
-    }).catch(error => console.log(`Failed to autoload controller: ${controllerName}`))
+    }).catch(error => console.log(`${error} Failed to autoload controller: ${controllerName}`))
   }
 }
 
